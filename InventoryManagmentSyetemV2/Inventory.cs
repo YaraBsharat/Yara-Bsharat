dd17905ebd7c28e956ff2f2e6b942a2e350e32db
@ybsharat <ybsharat@restaurant365.com>
2023-12-17 11:29:57 +0200

    Added a functionality of adding a product to the inventory

---
+++ InventoryManagmentSyetemV2/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagmentSyetemV2
+{
+    internal class Inventory
+    {
+        private List<Product> products;
+
+        public Inventory()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(string name, double price, int quantity)
+        {
+            Product newProduct = new Product(name, price, quantity);
+            products.Add(newProduct);
+            Console.WriteLine("Product added to inventory.");
+        }
+    }
+}
---
+++ InventoryManagmentSyetemV2/Program.cs
@@ -0,0 +1,28 @@
+﻿using InventoryManagmentSyetemV2;
+
+namespace YarasInventoryManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+
+            Console.WriteLine("Please Add a product");
+
+            Console.Write("Enter product name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter price: ");
+
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter quantity: ");
+
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            inventory.AddProduct(name, price, quantity);
+            
+        }
+    }
+}
